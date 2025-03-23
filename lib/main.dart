d88acf0743217742420bcbf3a1a22660ff3b4331
Allan Muikia <31766430+kibmuikia@users.noreply.github.com>
2025-03-24 00:01:37 +0300

    task: add util-setEquatableConfigStringify.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,35 @@
+import 'package:equatable/equatable.dart' show EquatableConfig;
 import 'package:flutter/foundation.dart' show kDebugMode;
 import 'package:flutter/material.dart';
 import 'package:kib_debug_print/kib_debug_print.dart' show DebugPrintService;
 import 'package:kib_theme_switcher/common_export.dart'
     show ThemeService, getIt, setupServiceLocator, LoginScreen;
 
+/*
+ * Sets the EquatableConfig stringify value.
+ * 
+ * @param {boolean} value - The value to set for EquatableConfig.stringify.
+ * @returns {void}
+ * 
+ * If stringify is overridden for a specific Equatable class, 
+ * then the value of EquatableConfig.stringify is ignored. 
+ * In other words, the local configuration always takes precedence over the global configuration.
+ * Note: EquatableConfig.stringify defaults to true in debug mode and false in release mode.
+ */
+void setEquatableConfigStringify(value) {
+  EquatableConfig.stringify = value;
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   DebugPrintService.initialize();
+
   // Initialize service locator
   await setupServiceLocator();
+
+  
+  setEquatableConfigStringify(true);
 
   runApp(const MyApp());
 }
